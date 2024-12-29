6061809d0a37f8722ab9ffef735f54c9697562c2
asmanov <asmanov.a@gmail.com>
2024-12-29 14:58:35 +0200

    Add project files.

---
+++ GarageApp.Client/Features/Home/Equipment.cs
@@ -0,0 +1,13 @@
+﻿namespace GarageApp.Client.Features.Home
+{
+    public class Equipment
+    {
+        public required string Id { get; set; }
+        public required string ModelName { get; set; }
+        public required string BrandName { get; set; }
+        public required string Image { get; set; }
+        public required string Description { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+    }
+}
