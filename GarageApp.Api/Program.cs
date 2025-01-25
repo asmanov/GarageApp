1e408b3d0272bea8754bb944e849c366a4f35341
asmanov <asmanov.a@gmail.com>
2025-01-25 17:37:28 +0200

    Revert "Revert "Add "Add equipment form"""

---
+++ GarageApp.Api/Program.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using GarageApp.Api.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<GarageAppContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("GarageAppConnectionString")));
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddControllers();
+builder.Services.AddValidatorsFromAssembly(Assembly.Load("GarageApp.Shared")); // Регистрация валидаторов
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+await app.RunAsync();
