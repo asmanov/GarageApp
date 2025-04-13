643b6ce445f17291d36cb72b559c2bb8ba7bcab1
asmanov <asmanov.a@gmail.com>
2025-04-13 08:42:53 +0300

    установка конечной даты адаптации тахо через два года

--- GarageApp.Shared/Features/ManageEquipment/TachoAction.cs
+++ GarageApp.Shared/Features/ManageEquipment/TachoAction.cs
@@ -1,4 +1,5 @@
-﻿using Supabase.Postgrest.Attributes;
+﻿using Newtonsoft.Json.Linq;
+using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
 
 namespace GarageApp.Shared.Features.ManageEquipment
@@ -6,10 +7,10 @@ namespace GarageApp.Shared.Features.ManageEquipment
     public class TachoAction : BaseModelApp
     {
         [Column("start")]
-        public DateOnly Start { get; set; }
+        public DateOnly Start { get; set; }/* = DateOnly.FromDateTime(DateTime.Now);*/
 
         [Column("end")]
-        public DateOnly End { get; set; }
+        public DateOnly End { get; set; }/* = DateOnly.FromDateTime(DateTime.Now.AddYears(2));*/
 
         [Column("track")]
         public int Track { get; set; }
