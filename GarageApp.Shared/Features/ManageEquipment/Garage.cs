8f726380027e53ac485d6e5159d91370b398f268
asmanov <asmanov.a@gmail.com>
2025-05-03 16:47:52 +0300

    Добавил автоматическое добавление имени файла

--- GarageApp.Shared/Features/ManageEquipment/Garage.cs
+++ GarageApp.Shared/Features/ManageEquipment/Garage.cs
@@ -1,4 +1,5 @@
-﻿using Supabase.Postgrest.Attributes;
+﻿using FluentValidation;
+using Supabase.Postgrest.Attributes;
 using Supabase.Postgrest.Models;
 
 namespace GarageApp.Shared.Features.ManageEquipment
@@ -7,7 +8,17 @@ namespace GarageApp.Shared.Features.ManageEquipment
     public class Garage : BaseModelApp
     {
         [Column("registration_number")]
-        public string RegistrationNumber { get; set; } = default!;
+        public string RegistrationNumber
+        {
+            get => _registrationNumber;
+            set
+            {
+                _registrationNumber = value;
+                Image = SetImageName(value);
+            }
+        }
+
+        private string _registrationNumber = string.Empty;
 
         [Column("description")]
         public string? Description { get; set; }
@@ -29,5 +40,41 @@ namespace GarageApp.Shared.Features.ManageEquipment
 
         [Column("site_id")]
         public int? SiteId { get; set; }
+
+        private static string SetImageName(string registrationNomber)
+        {
+            var saveName = registrationNomber;
+            saveName = saveName.Replace("/", "_").Replace(" ", "_").Replace(":", "_");
+            saveName = saveName + ".jpg";
+            return saveName;
+        }
+    }
+
+    public class GarageValidator : AbstractValidator<Garage>
+    {
+        public GarageValidator()
+        {
+            RuleFor(x => x.RegistrationNumber)
+                .NotEmpty()
+                .WithMessage("Ведіть державний номер")
+                .Length(8, 11)
+                .WithMessage("Державний номер має 8 символів");
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("Ведіть тип авто");
+            RuleFor(x => x.Year)
+                .InclusiveBetween(2000, 2025)
+                .WithMessage("Значення повино бути між 2000 та 2025")
+                .NotEmpty()
+                .WithMessage("Ведіть рік виробництва");
+            RuleFor(x => x.Vin)
+                .NotEmpty()
+                .WithMessage("Ведіть VIN номер")
+                .Length(17)
+                .WithMessage("VIN номер повинен мати 17 символів");
+            RuleFor(x => x.VolumeFuelTank)
+                .NotEmpty()
+                .WithMessage("Ведіть загальний об'єм паливного бака");
+        }
     }
 }
