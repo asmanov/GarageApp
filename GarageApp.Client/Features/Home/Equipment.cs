61a0cfa3071d33d68af06022090ba3d851319542
asmanov <asmanov.a@gmail.com>
2025-05-31 20:38:31 +0300

    Страховки плюс Тахограф плюс Техосмотр

--- GarageApp.Client/Features/Home/Equipment.cs
+++ GarageApp.Client/Features/Home/Equipment.cs
@@ -4,7 +4,7 @@ using Supabase.Postgrest.Models;
 
 namespace GarageApp.Client.Features.Home
 {
-    [Table("equipmens_view")]
+    [Table("garage_full_view")]
     public class Equipment : BaseModelApp
     {
         [Column("registration_number")]
@@ -33,5 +33,35 @@ namespace GarageApp.Client.Features.Home
 
         [Column("vin")]
         public string Vin { get; set; } = default!;
+
+        [Column("osago_start")]
+        public DateOnly? OsagoStart { get; set; }
+
+        [Column("osago_end")]
+        public DateOnly? OsagoEnd { get; set; }
+
+        [Column("kasko_start")]
+        public DateOnly? KaskoStart { get; set; }
+
+        [Column("kasko_end")]
+        public DateOnly? KaskoEnd { get; set; }
+
+        [Column("green_start")]
+        public DateOnly? GreenStart { get; set; }
+
+        [Column("green_end")]
+        public DateOnly? GreenEnd { get; set; }
+
+        [Column("tacho_start")]
+        public DateOnly? TachoStart { get; set; }
+
+        [Column("tacho_end")]
+        public DateOnly? TachoEnd { get; set; }
+
+        [Column("inspection_start")]
+        public DateOnly? InspectionStart { get; set; }
+
+        [Column("inspection_end")]
+        public DateOnly? InspectionEnd { get; set; }
     }
 }
