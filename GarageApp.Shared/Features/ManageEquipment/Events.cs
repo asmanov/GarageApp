77fa502351504340a0ce7939990b8708596424d8
asmanov <asmanov.a@gmail.com>
2026-03-07 14:41:29 +0200

    Add page Odometr

---
+++ GarageApp.Shared/Features/ManageEquipment/Events.cs
@@ -0,0 +1,31 @@
+﻿using Supabase.Postgrest.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarageApp.Shared.Features.ManageEquipment
+{
+    [Table("odometer_readings")]
+    public class Events : BaseModelApp
+    {
+        [Column("event_date")]
+        public DateOnly EventDate { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+
+        [Column("odometr")]
+        public int? Odometr {  get; set; }
+
+        [Column("event")]
+        public string EventItem { get; set; }
+
+        [Column("description")]
+        public string Description { get; set; }
+
+        [Column("equipment_id")]
+        public int Track {  get; set; }
+
+        [Column("cost")]
+        public double? Cost { get; set; }
+    }
+}
